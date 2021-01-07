0a89591807a49059691bacfd30f7540a9bf6cbc8
Ddiog-dev <ildegard42@hotmail.com>
2021-01-07 09:51:48 +0100

    Init commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into customer values(200,'The Tartar Steppe', 'test');
+insert into customer values(250,'The Tartar Steppe', 'test');
+insert into customer values(350,'The Tartar Steppe', 'test');
