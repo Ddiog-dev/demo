c84914beb3e5523064211aea603aa2f45878e44b
Ddiog-dev <ildegard42@hotmail.com>
2021-01-14 14:40:01 +0100

    Refactor pour ajouter une couche service Ajout du concept de DTO et conversion auto DTO vers JSON dans l'html Ajout des profils DEV et test ( DEV est sur la DB en docker pour l'instant)

---
+++ src/main/java/com/demo/DTO/CustomerDTO.java
@@ -0,0 +1,32 @@
+package com.demo.DTO;
+
+import org.springframework.stereotype.Component;
+
+
+public class CustomerDTO {
+    private  String name;
+    private  String email;
+
+    public CustomerDTO(){};
+
+    public CustomerDTO(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/test/java/com/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.demo;
 
+import com.demo.services.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    private final CustomerService customerService= new CustomerService();
 
 }
