75e7692bd92618a7d656012251abcbf4525b3991
Ddiog-dev <ildegard42@hotmail.com>
2021-01-15 11:19:26 +0100

    ajout test findAll()

--- src/test/java/com/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/DemoApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -26,17 +28,30 @@ public class DemoApplicationTests {
 
     @Test
     public void testDelete() {
-        customerService.save(new CustomerDTO("name","mail"));
-        customerService.deleteByName("name");
-        CustomerDTO customerDTO = customerService.findByName("name"); ;
+        customerService.save(new CustomerDTO("delete1","delete1"));
+        customerService.deleteByName("delete1");
+        CustomerDTO customerDTO = customerService.findByName("delete1");
         assert(customerDTO.getName().equals(""));
     }
 
     @Test
     public void testSaveAndFindByName(){
-        customerService.save(new CustomerDTO("name","mail"));
-        CustomerDTO customerDTOReturn = customerService.findByName("name");
+        customerService.save(new CustomerDTO("save1","save1"));
+        CustomerDTO customerDTOReturn = customerService.findByName("save1");
         // Verify that a new entry is made in DB and that we can have it back
-        assert(customerDTOReturn.getName().equals("name"));
+        assert(customerDTOReturn.getName().equals("save1"));
+
+        customerService.deleteByName("save1");
+    }
+
+    @Test
+    public void testAll(){
+        customerService.save(new CustomerDTO("all1","all1"));
+        customerService.save(new CustomerDTO("all2","all2"));
+
+        List<CustomerDTO> customers = customerService.findAll();
+        assert(customers.size() == 2);
+        assert(customers.get(0).getName().equals("all1"));
+        assert(customers.get(1).getName().equals("all2"));
     }
 }
